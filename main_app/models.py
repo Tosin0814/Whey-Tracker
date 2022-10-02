c40371e77fd0959aec0ba7f958eadf0f25e4817e
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-10-02 06:39:32 -0400

    Add templates and create model for main_app

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator, DecimalValidator
 
 # Create your models here.
+class Whey(models.Model):
+    name = models.CharField(max_length=300)
+    protein_content = models.FloatField(validators=[MinValueValidator(0.0), DecimalValidator(5, 1)],)
+    size = models.FloatField(validators=[MinValueValidator(1.0), DecimalValidator(5, 1)],)
+    price = models.FloatField(validators=[MinValueValidator(0.0), DecimalValidator(5, 2)],)
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(5.0), DecimalValidator(5, 1)],)
+    review = models.TextField(max_length=500)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+class Whey:
+    def __init__(self, name, protein_content, size, price, rating, review):
+        self.name = name
+        self.protein_content = protein_content
+        self.size = size
+        self.price = price
+        self.rating = rating
+        self.review = review
+
+whey = [
+    Whey('Sunshine Biopharma', 24, 5, 77, 4, 'Lean protein supplement with great taste and good values for money'),
+    Whey('Pure Protein Powder', 25, 5, 54, 4.5, 'Although the taste is not so great, it is very affordable and has a high protein content. It is difficult to find this product'),
+    Whey('Optimum Nutrition Gold Standard', 24, 5, 119, 3.8, 'High quality lean protein supplement used by athletes. It has a nice taste, but it is expensive compared to other brands on the market'),
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def whey_index(request):
+    return render(request, 'whey/index.html', { 'whey': whey})
+    
