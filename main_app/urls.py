fffc27e695039d081c40235e754a62810623af80
Tosin Ayanleye <100681205+Tosin0814@users.noreply.github.com>
2022-10-05 15:43:15 -0400

    Modify templates

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,4 @@
+from operator import ne
 from urllib.parse import urlparse
 from django.urls import URLPattern, path
 from . import views
@@ -5,6 +6,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('whey/', views.whey_index, name='index'),
-    path('whey/<int:whey_id>', views.whey_detail, name='detail')
+    path('whey/', views.WheyList.as_view(), name='index'),
+    path('whey/<int:pk>/', views.WheyDetail.as_view(), name='detail'),
+    path('whey/create', views.WheyCreate.as_view(), name='whey_create'),
+    path('whey/<int:pk>/update/', views.WheyUpdate.as_view(), name='whey_update'),
+    path('whey/<int:pk>/delete/', views.WheyDelete.as_view(), name='whey_delete')
 ]
